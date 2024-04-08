4a52489b2cd5f4ecea5067425b11dbcf431fca54
SamyakBorkar <2022bit037@sggs.ac.in>
2024-04-08 16:44:09 +0530

    Exception handeling updated

--- practise code/EH2.java
+++ practise code/EH2.java
@@ -1,11 +1,30 @@
 class Examplee{
+private int a ;
+private int b ;
+
+
+	public void arithmeticException(int a , int b){
+		this.a=a;
+		this.b=b;
+		
+		try{
+			int  result = a/b;
+		}
+		catch(Exception e){
+			System.out.println("Cant divide by zero");
+		}
+		
+	}
 	public static void main(String ...args){
 		try{
 			System.out.println(args[0]);
 		}
 		catch(Exception e){
+		
+			Examplee e1 =new Examplee();
 			System.out.println("We are indexing the 0 array");
 			System.out.println("The Exception is  Due to:"+e);
+			e1.arithmeticException(4,0);
 			
 			}
 	}
