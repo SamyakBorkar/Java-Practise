3be6f67c628311ff930361432e993aa29bf47790
SamyakBorkar <2022bit037@sggs.ac.in>
2024-04-16 22:20:57 +0530

    Null Pointer Exception Code done

---
+++ practise code/NullPointerExcep.java
@@ -0,0 +1,13 @@
+class MEMO{
+	public static void main(String args[]){
+		int arr[] =null;
+		
+		try{
+		System.out.println(arr[0]);
+		}
+		
+		catch(Exception ex){
+			System.out.println("Exception"+ex);
+		}
+	}
+}
